271bbbc416787a0cc7f0eb2156197796dac49017
MuskanAkter <87993017+MuskanAkter@users.noreply.github.com>
2022-07-29 12:20:14 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+// task 1
+/* var firstName , lastName , fullName;
+firstName = prompt("Enter your First name : ");
+lastName = prompt("Enter the last name :");
+fullName = firstName +  lastName ;
+document.write(fullName);
+var len = fullName.length;
+document.write(len);
+document.write(fullName.toUpperCase()); */
+
+
+// Task 4 : letter grade
+/*-----------------
+var marks = prompt("Enter your marks :");
+if(marks >= 80 && marks <=100)
+console.log(" You have got an A+");
+
+else if(marks >= 70 && marks <=79)
+console.log(" You have got  A");
+
+else if(marks >= 60 && marks <=69)
+console.log(" You have got  A-");
+
+else if(marks >= 50 && marks <=59)
+console.log(" You have got  B");
+
+else if(marks >= 40 && marks <=49)
+console.log(" You have got  C");
+
+
+else if(marks >= 33 && marks <=0)
+console.log(" You have got  D");
+
+else 
+console.log("You are fail");
+----------*/
+/* ------------- For loop
+var text =["hi","hello", "excuse me"]
+var i = text.length
+for(i = 0; i<4; i++) {
+    document.write(text[i] );
+}
+--------------------*/
+/*---------1+2+3+4+5+........10  using for loop 
+var sum =0;
+for(var i = 0; i<=10; i++){
+    sum=sum +i;
+}
+document.write(sum);
+------------*/
+
+
+
+
+
+
